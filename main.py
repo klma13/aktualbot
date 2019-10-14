f52fd1bfe4032f3568dadddf9852c139c1a57c68
Filip Troníček <filip.tronicek@seznam.cz>
2019-10-14 17:24:22 +0200

    pretty print the articles

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # Import libraries
+
+
 import requests
 import urllib.request
 import time
@@ -18,7 +20,14 @@ for cell in soup.select('div.g_f1 > ul > li > div > div > div > a:nth-child(1)')
   responseAr = requests.get(cell['href'])
   soupAr = BeautifulSoup(responseAr.text, "html.parser")
   for textDiv in soupAr.select('div.f_cH > p:nth-child(1)'):
-    print(textDiv.getText())
-    
+    articleText += str(textDiv)
 
+  print(articleText)
+  print("\n ====================================== \n")
 
+  emailReUrl = "https://hook.integromat.com/9k5llgs07x7mzm4m89g7l6fdt40p8o3g"
+  data = {
+    "email": "filip.tronicek@seznam.cz",   
+    "msg" : articleText
+  }
+  #requests.post(emailReUrl, data=data, allow_redirects=True)
