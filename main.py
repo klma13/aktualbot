1617a5ad82e476fbef0446216b2d55688cc31aeb
Filip Troníček <filip.tronicek@seznam.cz>
2019-10-14 18:42:26 +0200

    Change email every time

--- main.py
+++ main.py
@@ -5,13 +5,22 @@ import requests
 import urllib.request
 import time
 from bs4 import BeautifulSoup
+import re
+
+from emails import emails
 
 # Set the URL you want to webscrape from
 url = 'https://www.novinky.cz/zahranicni/svet'
+
+
 print("Making a request to",url)
+
 # Connect to the URL
 response = requests.get(url)
 print("Got index")
+
+emailIndex = 0
+
 # Parse HTML and save to BeautifulSoup object
 soup = BeautifulSoup(response.text, "html.parser")
 
@@ -19,25 +28,35 @@ for cell in soup.select('div.g_f1 > ul > li > div > div > div > a:nth-child(1)')
   articleText = ""
   print(cell['href'])
   print("\n")
-  time.sleep(0.2)
 
   responseAr = requests.get(cell['href'])
   soupAr = BeautifulSoup(responseAr.text, "html.parser")
 
   for textDiv in soupAr.select('h1.d_w'):
-    articleText += "<h1>"+str(textDiv) + "</h1>"
+    articleText += "<h1>"+str(textDiv.getText()) + "</h1>"
+    heading = str(textDiv)
   for textDiv in soupAr.select('.g_fY > p:nth-child(1)'):
     articleText += str(textDiv)
 
   for textDiv in soupAr.select('div.f_cH > p:nth-child(1)'):
     articleText += str(textDiv)
 
-  print(articleText)
+  #print(articleText)
   print("\n ====================================== \n")
 
   emailReUrl = "https://hook.integromat.com/9k5llgs07x7mzm4m89g7l6fdt40p8o3g"
   data = {
-    "email": "filip.tronicek@seznam.cz",   
+    "email": emails[emailIndex],
     "msg" : articleText
   }
-  requests.post(emailReUrl, data=data, allow_redirects=True)
+  heading = heading.replace('<h1 class="d_w d_x">', '')
+  heading = heading.replace('</h1>', '')
+
+  print("Sending", heading,"to",data['email'])
+  emailIndex += 1
+  if len(emails) == emailIndex:
+    break
+  #requests.post(emailReUrl, data=data, allow_redirects=True)
+
+if len(emails) < emailIndex:
+  print("Bruh need moar")
